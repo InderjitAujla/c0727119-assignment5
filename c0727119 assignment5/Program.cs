4a40365fb2edcbc327db8afd412fa5e083218463
visualstudio <visualstudio@230-PC42>
2019-04-18 15:35:57 -0400

    commit2

--- c0727119 assignment5/Program.cs
+++ c0727119 assignment5/Program.cs
@@ -10,35 +10,44 @@ namespace c0727119_assignment5
     {
         static void Main(string[] args)
         {
-            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
-            int arr_size = arr.Length;
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
-
-            printRepeating(arr, arr_size);
-
+            Program a = new Program();
+            Console.WriteLine("the longest word is : ");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
         }
-        public static void printRepeating(int[] arr, int size)
+
+        public String MyFunctionA(string input)
         {
-            int i, j;
-            int counter = 0;
-            Console.WriteLine("Occurs 3 times the array elements are: ");
-            for (i = 0; i < size; i++)
+            //Write a C# program to find the longest word in a string.
+            string[] words = input.Split(' ');
+            int wordarrayLength = words.Length;
+            String longestword = words[0];
+            int[] wordsLength = new int[words.Length];
+            for (int s = 0; s < words.Length; s++)
             {
-                for (j = i + 1; j < size; j++)
-                {
-                    if (arr[i] == arr[j])
-                    {
-                        counter++;
-                        Console.WriteLine(arr[i]);
-                    }
-
-                }
+                wordsLength[s] = words[s].Length;
             }
+
+
+            string currentWord = words[0];
+            string nextWord;
+
+            for (int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+
+                nextWord = words[y + 1];
+                if (words[y + 1].Equals(words[y]))
+                {
+                    longestword = words[y + 1];
+                }
+
+
+            }
+            return longestword;
+
         }
+
 
     }
 }
+
